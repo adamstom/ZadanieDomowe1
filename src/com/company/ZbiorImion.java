2800a814a611526743cb98a8461ff9e4410a06d8
Piotr Goetz <goetzpiotr@gmail.com>
2018-07-29 19:21:38 +0200

    Zrobione zad 10

---
+++ src/com/company/ZbiorImion.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ZbiorImion {
+
+    private Set<String> set = new HashSet<>();
+    private String imie = "";
+
+    public Set<String> getSet() {
+        return set;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void zapiszImie(String imie) {
+        if (set.add(imie)) {
+            System.out.println("Dodano imie: " + imie + " do listy");
+        } else {
+            System.out.println("Imie jest juz w bazie danych");
+        }
+    }
+}
